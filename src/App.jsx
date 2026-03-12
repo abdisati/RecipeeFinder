3ae6f98aafd125c020407bf2675711ca108b65b1
Abdi Deresa <abdishegute@gmail.com>
2026-03-11 17:35:55 -0700

    import Recipee class

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,12 @@
+import { Recipee } from "./utility/recipee";
 
-//  export const meals = [
-//   {
-//     id: "52795",
-//     strMeal: "Chicken Handi",
-//     strCategory: "Chicken",
-//     strArea: "Indian",
-//     strMealThumb: "https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg",
-//   },
-//   {
-//     id: "53358",
-//     strMeal: "Chicken Mandi",
-//     strCategory: "Chicken",
-//     strArea: "Indian",
-//     strMealThumb: "https://www.themealdb.com/images/media/meals/er4d081765186828.jpg",
-//   },
-//   {
-//     id: "53001",
-//     strMeal: "Butter Chicken",
-//     strCategory: "Chicken",
-//     strArea: "Indian",
-//     strMealThumb: "https://www.themealdb.com/images/media/meals/qptpvt1487339892.jpg",
-//   },
-//   {
-//     id: "53002",
-//     strMeal: "Palak Paneer",
-//     strCategory: "Vegetarian",
-//     strArea: "Indian",
-//     strMealThumb: "https://www.themealdb.com/images/media/meals/bc8v651619789840.jpg",
-//   },
-//   {
-//     id: "53003",
-//     strMeal: "Lamb Rogan Josh",
-//     strCategory: "Lamb",
-//     strArea: "Indian",
-//     strMealThumb: "https://www.themealdb.com/images/media/meals/vvtvtr1511180578.jpg",
-//   },
-//   {
-//     id: "53004",
-//     strMeal: "Dal Makhani",
-//     strCategory: "Vegetarian",
-//     strArea: "Indian",
-//     strMealThumb: "https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg",
-//   },
-// ];
-
-const data={
-  id:"53004",
-  meal:"Dal Makhani",
-  category:"vegeterian",
-  area:"Indian",
-  thumb:"https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg"
-}
+// const data={
+//   id:"53004",
+//   meal:"Dal Makhani",
+//   category:"vegeterian",
+//   area:"Indian",
+//   thumb:"https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg"
+// }
 
 function SearchBar(){
   return <div className="flex justify-around m-4 px-2 gap-2">
